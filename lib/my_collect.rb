6476dcd2d43a01caa952cd690db584453d87860a
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-09 02:19:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(collection)
 
   new_collection = []
   while i < collection.length do
-    new_collection << yield[collection[i]]
+    new_collection << yield(collection[i])
     i += 1
   end
 
